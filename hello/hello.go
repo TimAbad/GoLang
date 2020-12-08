b6cccbf8a62e9de23e51f6f7ee77894e1d3dfb8d
TimAbad <timothy.abad.611@my.csun.edu>
2020-12-08 10:02:56 -0800

    A5 Return greetings for multiple people using a slice and map

--- hello/hello.go
+++ hello/hello.go
@@ -14,15 +14,18 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	// Request a greeting message.
-	message, err := greetings.Hello("Tim")
-	// If an error was returned, print it ot the console and
+	// A slice of names
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Request greetubg nessages for the names.
+	message, err := greetings.Hellos(names)
+	// If an error was returned, print it out the console and
 	// exit the program.
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// If no error was returned, print the returned message
-	// to the console
+	// If no error was returned, print the returned map of
+	// messages to the console
 	fmt.Println(message)
 }
